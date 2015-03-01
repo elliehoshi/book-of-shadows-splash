db5f528371feecc5ca8a4c884f8ab7ee5ae03f56
Ellie Hoshizaki <ellie@mediahound.com>
2015-02-28 23:25:23 -0800

    refactored logo icon hover animation

--- source/js/main.js
+++ source/js/main.js
@@ -19,6 +19,35 @@ var animateInSvgLogo = function(){
 	// }, 2000);
 };
 
+var animateOutSvgLogo = function(){
+	var path = document.querySelector('#logo');
+	var length = path.getTotalLength();
+	// Clear any previous transition
+	// path.style.transition = path.style.WebkitTransition =
+	  'none';
+	// Set up the starting positions
+	path.style.strokeDasharray = length + ' ' + length;
+	path.style.strokeDashoffset = '0';
+	// Trigger a layout so styles are calculated & the browser
+	// picks up the starting position before animating
+	path.getBoundingClientRect();
+	// Define our transition
+	path.style.transition = path.style.WebkitTransition =
+	  'stroke-dashoffset 0.5s ease';
+	// Go!
+	// setTimeout(function(){ 
+		path.style.strokeDashoffset = length; 
+	// }, 2000);
+};
+
 window.onload = function(){
 	animateInSvgLogo();
 };
+
+$( ".bs-logo" ).hover(
+  function() {
+    animateOutSvgLogo();
+  }, function() {
+    animateInSvgLogo();
+  }
+);
